262ea6ae0782d7b3f8dbadf3f06f1b1f39f948a9
Indra Puji Novirwan <indrapuji@gmail.com>
2022-07-31 19:24:21 +0700

    done

--- src/screens/ScanScreen.js
+++ src/screens/ScanScreen.js
@@ -10,12 +10,20 @@ import {
 import React from 'react';
 import {leftArrow} from '../assets';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
 const {width} = Dimensions.get('screen');
 const ScanScreen = ({navigation}) => {
+  const onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
   return (
     <SafeAreaView>
       <View style={{marginLeft: 16, marginRight: 16}}>
-        <View
+        {/* <View
           style={{
             position: 'relative',
             justifyContent: 'center',
@@ -90,7 +98,24 @@ const ScanScreen = ({navigation}) => {
           <Text style={{fontSize: 20, fontWeight: '700', color: 'white'}}>
             Scan Barcode
           </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
+
+        <QRCodeScanner
+          onRead={onSuccess}
+          flashMode={RNCamera.Constants.FlashMode.torch}
+          topContent={
+            <Text style={styles.centerText}>
+              Go to{' '}
+              <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+              your computer and scan the QR code.
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+          }
+        />
       </View>
     </SafeAreaView>
   );
@@ -98,4 +123,22 @@ const ScanScreen = ({navigation}) => {
 
 export default ScanScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
